13233d91e83c1b0bb5739f581675fff25af3fd9e
Timothy Lock <lock.k.timothy@gmail.com>
2014-12-06 14:39:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from modules.datasave import datasave
 import time
 
 #User Variables
-username = "arexxk"
+username = "arexxkd"
 
 teamInfo.getTeam(username)
 
 start = datasave.read("data/teaminfo.dat")
 
-while start[0] == "nogame":
-    print("Player not in game / Game not observable")
-    time.sleep(5) #time delay in seconds
-    teamInfo.getTeam(username)
-    start = datasave.read("data/teaminfo.dat")
-
+try:
+    while start[0] == "nogame":
+        print("Player not in game / Game not observable")
+        time.sleep(5) #time delay in seconds
+        teamInfo.getTeam(username)
+        start = datasave.read("data/teaminfo.dat")
+except:
+    print
 print("Game has started")
+

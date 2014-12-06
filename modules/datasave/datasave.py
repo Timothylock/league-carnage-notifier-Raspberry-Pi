0f38a154e5b4f80e8f8db880d7f3fbc46a9cb930
Timothy Lock <lock.k.timothy@gmail.com>
2014-12-06 10:15:38 -0500

    Final
    Usage:
    
    To Save:
    import datasave
    datasave.save(dict, string of desired filename)
    
    To Read:
    import datasave
    foo = datasae.read(string of desired filename)

--- modules/datasave/datasave.py
+++ modules/datasave/datasave.py
@@ -1,1 +1,12 @@
 import json
+
+def save(dic, filename):
+    f = open(filename,'w')
+    f.write(json.dumps(dic))
+    f.close()
+
+def read(filename):
+    f = open(filename, 'r')
+    dic = json.loads(f.readline())
+    f.close()
+    return dic
